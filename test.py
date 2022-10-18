641b1dfd23ca104aad7ceab599dfc836892970e2
LEEHM97 <gmlkd2006@gmail.com>
2022-10-19 01:12:15 +0900

    feat: show images original, predicted

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 import os
 import argparse
 import torch
-import wandb
-import glob
-import natsort
 import cv2
 
 import torch.nn as nn
 import numpy as np
 import pytorch_lightning as pl
+import matplotlib.pyplot as plt
 
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -26,11 +24,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=42)
 parser.add_argument("--num_workers", type=int, default=4)
 parser.add_argument("--project", type=str, default="Crack_Detection")
-parser.add_argument("--name", type=str, default="unet++_b0")
+parser.add_argument("--name", type=str, default="unet++_b1")
 parser.add_argument("--model", type=str, default="UnetPlusPlus")
-parser.add_argument("--encoder", type=str, default="efficientnet-b0")
+parser.add_argument("--encoder", type=str, default="efficientnet-b1")
 parser.add_argument("--precision", type=int, default=16)
-parser.add_argument("--idx", type=int, default=0)
 
 
 # training 설정
@@ -53,40 +50,41 @@ parser.add_argument('opts',
                     nargs=argparse.REMAINDER)
 
 args = parser.parse_args()
-train_data_dir = "./Datasets/Train"
-test_data_dir = "./Datasets/Test"
 
 if __name__ == "__main__":
-    pl.seed_everything(args.seed)
-    train_images = np.array(natsort.natsorted(glob.glob(os.path.join(train_data_dir, "images", "*"))))
-    train_masks = np.array(natsort.natsorted(glob.glob(os.path.join(train_data_dir, "masks", "*"))))
-
-    test_images = np.array(natsort.natsorted(glob.glob(os.path.join(test_data_dir, "images", "*"))))
-    test_masks = np.array(natsort.natsorted(glob.glob(os.path.join(test_data_dir, "masks", "*"))))
+    image_dir = "./Datasets/Test/images/20160308_072824.jpg"
+    mask_dir = "./Datasets/Test/masks/20160308_072824_mask.png"
+    model_dir = "./checkpoints/unet++_f1=0.8276.ckpt"
     
-    train_transform, test_transform = make_transform(args)
-    model = SegmentationModel(args)
-    # model = SegmentationModel.load_from_checkpoint('checkpoints\\unet++_b0_fold02_val\\f1=0.0000-v2.ckpt', args)
-    
+    _, test_transform = make_transform(args)
+    model = SegmentationModel.load_from_checkpoint(model_dir, args=args)
     model.freeze()
-    test_ds = SegmentationDataset(test_images, test_masks, test_transform)
-    test_dataloader = torch.utils.data.DataLoader(
-        test_ds, batch_size=1, num_workers=args.num_workers)
     
     trainer = pl.Trainer(accelerator='gpu',
                         devices=1,
-                        max_epochs=5,
-                        log_every_n_steps=1)
-    idx = args.idx
-
-    img, mask = test_ds[idx]
-    new_img = img.unsqueeze(0)
-
-    outputs = model(new_img)
+                        max_epochs=1,
+                        log_every_n_steps=1)    
     
-    origin_output = torch.sigmoid(outputs).squeeze(0).squeeze(0).numpy()
-    output = np.where(origin_output<0.6, 0, 255) 
-    outputs = outputs.numpy()
+    image = cv2.imread(image_dir)
+    mask = cv2.imread(mask_dir, 0)
     
-    cv2.imwrite('./outputs/origin_output.png', origin_output)
-    cv2.imwrite('./outputs/bw_output.png', output)
+    transformed_image = test_transform(image=image)
+    transformed_image = transformed_image['image'].float()
+    
+    preprocessed_image = transformed_image.unsqueeze(0)
+    outputs = model(preprocessed_image)
+    output = torch.sigmoid(outputs).squeeze(0).squeeze(0).numpy()
+    output = np.where(output<0.5, 0, 255)
+    
+    cv2.imwrite('./outputs/predicted_mask.png', output)
+    
+    output = np.array(output, dtype=np.uint8)
+    
+    mask = cv2.resize(mask, (1000,600))
+    output = cv2.resize(output, (1000, 600))
+    
+    cv2.imshow('original', mask)
+    cv2.imshow('predicted', output)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
