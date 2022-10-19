21868762fab7666c527a9647c27488f25a4ac651
LEEHM97 <gmlkd2006@gmail.com>
2022-10-20 01:20:01 +0900

    feat: save width image

--- inference.py
+++ inference.py
@@ -3,17 +3,10 @@ import argparse
 import torch
 import cv2
 
-import torch.nn as nn
 import numpy as np
 import pytorch_lightning as pl
-import matplotlib.pyplot as plt
 
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from sklearn.model_selection import KFold, StratifiedKFold, GroupKFold
-
-from datasets import SegmentationDataset
+import utils
 from transforms import make_transform
 from models import SegmentationModel
 
@@ -58,28 +51,18 @@ if __name__ == "__main__":
     _, test_transform = make_transform(args)
     model = SegmentationModel.load_from_checkpoint(model_dir, args=args)
     model.freeze()
-    
-    trainer = pl.Trainer(accelerator='gpu',
-                        devices=1,
-                        max_epochs=1,
-                        log_every_n_steps=1)    
-    
-    image = cv2.imread(image_dir)
-    
-    transformed_image = test_transform(image=image)
-    transformed_image = transformed_image['image'].float()
-    
-    preprocessed_image = transformed_image.unsqueeze(0)
-    outputs = model(preprocessed_image)
-    output = torch.sigmoid(outputs).squeeze(0).squeeze(0).numpy()
-    output = np.where(output<0.6, 0, 255)
+
+    output = utils.get_output(image_dir, model, test_transform)
     
     cv2.imwrite('./outputs/predicted_mask.png', output)
     
-    output = np.array(output, dtype=np.uint8)
-    output = cv2.resize(output, (1000, 600))
+    skel = utils.skeletonize(output)
+    canny = utils.canny(output)
     
-    cv2.imshow('predicted', output)
+    cv2.imwrite('./outputs/skel.png', skel)
+    cv2.imwrite('./outputs/canny.png', canny)
     
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    pixel_pairs, distance = utils.get_width(skel, canny)
+    max_width_idx, max_width = utils.get_max_width(distance)
+    
+    utils.visualize_width(output, pixel_pairs, max_width_idx)
