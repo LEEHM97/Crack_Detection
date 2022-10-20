ca25f8c86f38a855d7cd7aa70954996b68484838
LEEHM97 <gmlkd2006@gmail.com>
2022-10-20 15:55:05 +0900

    del: hrnet files

--- practice/save.py
+++
@@ -1,7 +0,0 @@
-import os
-import torch
-
-
-def save_model(model_state, model_name, save_dir="./trained_model"):
-    os.makedirs(save_dir, exist_ok=True)
-    torch.save(model_state, os.path.join(save_dir, model_name))
